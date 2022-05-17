1d8f34d117432074f8e54492e6cfe52a9d44086d
Wallace Caldeira <wallace.caldeira@cwi.com.br>
2022-05-17 20:48:41 -0300

    str-producer started

---
+++ str-producer/src/main/java/com/wallace/strproducer/StrproducerApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrproducerApplication.class, args);
+	}
+
+}
