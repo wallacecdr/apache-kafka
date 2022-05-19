644bb200f34febd32bc0df3fe6e3494e58e5cee2
Wallace Caldeira <wallace.caldeira@cwi.com.br>
2022-05-18 21:35:21 -0300

    add callback function to send message funcion

---
+++ str-producer/src/main/java/com/wallace/strproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,33 @@
+package com.wallace.strproducer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    @Autowired
+    public KafkaProperties properties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topcs() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("str-topic").partitions(2).replicas(1).build()
+        );
+    }
+}
