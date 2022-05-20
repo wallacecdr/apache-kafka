53ad9a05c531877bab972b43cb87d5ff904ae2fa
Wallace Caldeira <wallace.caldeira@cwi.com.br>
2022-05-19 23:10:52 -0300

    error handler

--- str-consumer/src/main/java/com/wallace/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/wallace/strconsumer/listeners/StrConsumerListener.java
@@ -1,18 +1,20 @@
 package com.wallace.strconsumer.listeners;
 
 import com.wallace.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
